2c9c5be040183334e4b8c297e3867c0413ff6285
a_maulana <cintamupalsu@hotmail.com>
2017-11-10 06:16:57 +0900

    add basic pagination

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,14 +1,17 @@
 class ChaptersController < ApplicationController
+  PAGE_SIZE = 10
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords]
       chapter_search_term = ChapterSearchTerm.new(@keywords)
       @chapters = Chapter.where(
         chapter_search_term.where_clause,
         chapter_search_term.where_args).
-        order(chapter_search_term.order)
+        order(chapter_search_term.order).
+        offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
-      @chapters = Chapter.all.limit(10)
+      @chapters = Chapter.all.offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     end
   end
 end
